7708da7e25a9daa7073c95ccaf5283b711a105ad
tjweber1102 <38363326+tjweber1102@users.noreply.github.com>
2021-12-14 13:45:18 +0100

    added trigger points 4-9 + a little bit of documentation

--- UnityProject/Assets/Scripts/EventManager.cs
+++ UnityProject/Assets/Scripts/EventManager.cs
@@ -7,7 +7,6 @@ public class EventManager : MonoBehaviour
 {
 
     public static EventManager instance;
-    private bool started = false;
     private GameObject _next; 
 
     void Awake()
@@ -17,18 +16,35 @@ public class EventManager : MonoBehaviour
 
     private void Start()
     {
+        // TODO: maybe nicer to call RobotAt from somewhere else (to keep independence)
         _next = GameObject.Find("Trigger 1");
+        RobotAt(0, _next);
     }
 
-    private void Update()
-    {
-        // move first event to start function 
-        if (!started)
-        {
-            started = true;
-            RobotAt(0, _next);
-        }
-    }
+    /**
+     * TRIGGER ROBOT AT:
+     * 
+     * id == 2:
+     * play introduction
+     * maybe let the robot ask, whether the participant is ready to follow;
+     * can confirm with a button press -> triggers robot to move to trigger point 3
+     * 
+     * id == 3:
+     * play instruction 1: step over
+     * end of instructions triggers robot to go to trigger point 4
+     * 
+     * id == 4:
+     * wait for participant to step over the rubble and trigger the next step, maybe 
+     * turn towards the participant while waiting; the participant successfully stepping
+     * over the the rubble triggers the robot to move to trigger point 5; also triggers
+     * the robot to say something encouraging/approving?
+     * 
+     * id == 5-8: 
+     * moving to the next instruction point via next
+     * 
+     * id == 9:
+     * 
+     */
 
     public event Action<int, GameObject> robotAt;
     public void RobotAt(int id, GameObject next)
--- UnityProject/Assets/Scripts/TriggerRobotPosition.cs
+++ UnityProject/Assets/Scripts/TriggerRobotPosition.cs
@@ -8,6 +8,21 @@ public class TriggerRobotPosition : MonoBehaviour
     public int triggerPointId;
     public GameObject next;
 
+    /**
+     * TRIGGERS:
+     * 1: Intermediate point on the way to the participant -> change direction, go to point 2
+     * 2: In front of the participant (starting point)     -> play intro
+     * 3: In front of the rubble                           -> play instruction 1, move to point 4
+     * 4: Behind the rubble                                -> wait for the participant, maybe turn towards him
+     * 5: Intermediate point on the way to instruction 2   -> change direction, go to point 6
+     * 6: Intermediate point on the way to instruction 2   -> change direction, go to point 7
+     * 7: Intermediate point on the way to instruction 2   -> change direction, go to point 8
+     * 8: Intermediate point on the way to instruction 2   -> change direction, go to point 9
+     * 9: In front of toppled shelf                        -> play instruction 2, move to point 10
+     */
+
+    // TODO: Add ids/tags to player and robot and check whether id/tag == robot
+
     private void OnTriggerEnter(Collider other)
     {
         EventManager.instance.RobotAt(triggerPointId, next);
