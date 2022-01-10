ce2a89cb4847c97fc3f8f3a0ab356db70b55c1a0
jlinkemeyer <jlinkemeyer@uni-osnabrueck.de>
2022-01-10 18:48:05 +0100

    add beacon for red light (dramatic effects)

---
+++ UnityProject/Assets/Scripts/LightRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightRotation : MonoBehaviour
+{
+    public float rotationSpeed = 200F;
+    
+    void Update()
+    {
+        // Rotate the object around itself at a given rotation speed
+        transform.Rotate(Vector3.up * (rotationSpeed * Time.deltaTime), Space.Self);
+    }
+}
