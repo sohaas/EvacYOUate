5ff50317b3f04048a50b5b20752501ecbc17cf9e
sohaas <46130334+sohaas@users.noreply.github.com>
2022-01-17 01:49:38 +0100

    added functionality (movement, hands, custom buttons, etc.)

--- UnityProject/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ UnityProject/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,22 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Vector2 ContinuousMove
+        {
+            get
+            {
+                return SteamVR_Actions.default_ContinuousMove;
+            }
+        }
+        
+        public virtual SteamVR_Action_Boolean Next
+        {
+            get
+            {
+                return SteamVR_Actions.default_Next;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
