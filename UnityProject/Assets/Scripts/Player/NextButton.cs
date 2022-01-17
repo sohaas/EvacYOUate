571bdea458918dcf924d56c674d9e44f07ec64d1
sohaas <46130334+sohaas@users.noreply.github.com>
2022-01-17 15:22:28 +0100

    added player to the scene

---
+++ UnityProject/Assets/Scripts/Player/NextButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class NextButton : MonoBehaviour
+{
+    public SteamVR_Action_Boolean input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If trigger is pressed on one of the controllers
+        if (input.GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            // TODO switch scene (should only be possible in 1st scene after intro is finished)
+        }
+    }
+}
