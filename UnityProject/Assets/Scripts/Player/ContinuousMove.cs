c8b708316e1024af731843e16e2f560c4ec3d082
sohaas <46130334+sohaas@users.noreply.github.com>
2022-01-18 12:15:11 +0100

    added player

---
+++ UnityProject/Assets/Scripts/Player/ContinuousMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class ContinuousMove : MonoBehaviour
+{
+    public SteamVR_Action_Vector2 input;
+    public float speed = 1;
+    private CharacterController characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Prevent teleportation + continuous movement from interfering with each other
+        if (input.axis.magnitude > 0.1f)
+        {
+            Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
+            characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane( direction, Vector3.up) - new Vector3(0, 9.81f, 0) * Time.deltaTime);
+        }
+    }
+}
