0b5e7f2701727dd34d2f37d9a2d85211dd7fc80f
tjweber1102 <38363326+tjweber1102@users.noreply.github.com>
2022-01-26 21:30:20 +0100

    Put condition assignment in familiarization phase

---
+++ UnityProject/Assets/Scripts/Conditions/ConditionData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ConditionData
+{
+    public int[] conditions;
+    public int participant;
+
+    public ConditionData(ConditionManager condManager)
+    {
+        // TODO: replace 4 by const
+        participant = condManager.participant;
+        conditions = new int[4];
+
+        for (int i = 0; i < 4; i++)
+        {
+            conditions[i] = condManager.conditionCounter[i];
+        }
+    }
+}
